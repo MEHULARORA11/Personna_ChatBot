28400d0d56131f1a25412e7862b619ae63aa6a14
MEHULARORA11 <mehularora505@gmail.com>
2026-07-05 03:16:24 +0530

    feat:add app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,146 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import cookieParser from 'cookie-parser'
+import type {Request,Response} from 'express'
+import {OpenAI} from 'openai'
+import {run} from '@openai/agents'
+import type {Agent} from '@openai/agents' 
+import {hiteshAgent} from './agents/hitesh/agent.ts'
+import {HITESH_SIR_SYSTEM_PROMPT} from './agents/hitesh/Persona.ts'
+import {hiteshGuardRailAgent} from './agents/hitesh/guardrailAgent.ts'
+import {piyushAgent} from './agents/piyush/agent.ts'
+import {PIYUSH_SIR_SYSTEM_PROMPT} from './agents/piyush/Persona.ts'
+import {piyushGuardRailAgent} from './agents/piyush/guardrailAgent.ts'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+dotenv.config({ path: resolve(__dirname, '../../.env') })
+
+const client = new OpenAI()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors({
+    origin:["http://localhost:5173","https://personabot.mehularora.dev"],
+    credentials:true,
+}))
+app.use(cookieParser())
+
+async function getConverstaionId():Promise<string>{
+ const {id} = await client.conversations.create({})
+ return id
+}
+type GuardRailOutput = {
+  isValidQuery:boolean,
+  reason:string
+}
+async function main(
+    personiqAgent:Agent,
+    guardRailAgent:Agent<any,any>,
+    SYSTEM:string,
+    res:Response,
+    id:string,
+    userInput:string){
+    const guardRailResponse = await run(guardRailAgent,[
+        {
+        role:"user",
+        content:userInput
+       }
+])
+
+if(!guardRailResponse?.finalOutput?.isValidQuery){
+throw new Error(`Invalid Querry , due to Reason => ${guardRailResponse?.finalOutput?.reason}`)
+}
+    
+ const response = await run(personiqAgent,[
+        {
+        role:"system",
+        content:SYSTEM
+       },
+        {
+        role:"user",
+        content:userInput
+       }
+],{
+    stream:true,
+    conversationId:id
+})
+
+const streamOutput = response.toTextStream()
+
+for await (const chunk of streamOutput){
+    res.write(chunk)
+}
+res.end()
+
+}
+
+app.post('/api/post',async (req:Request,res:Response) => {
+
+    const {message,persona} = req.body
+
+           if(!message || !persona){
+            throw new Error('invalid message or persona')
+           }
+          let personaId_1 = null
+           switch (persona) {
+            case "hitesh":{
+                personaId_1 = req.cookies?.hiteshAgentId_1
+                 console.log(personaId_1)
+                if(!personaId_1){
+              const id = await getConverstaionId()
+                res.cookie("hiteshAgentId_1",id,{
+                    httpOnly:true,
+                    sameSite:'lax',
+                    secure:false,
+                    maxAge: 30 * 1000 // 30 second
+               })
+
+                  return await main(
+                    hiteshAgent,
+                    hiteshGuardRailAgent,
+                    HITESH_SIR_SYSTEM_PROMPT,
+                    res,id,message)
+           }
+
+                return await main(
+                hiteshAgent,
+                hiteshGuardRailAgent,
+                HITESH_SIR_SYSTEM_PROMPT,
+                res,personaId_1,message)
+
+            }
+            case "piyush":{
+                personaId_1 = req.cookies?.piyushAgentId_1
+                 console.log(personaId_1)
+                    if(!personaId_1){
+                const id = await getConverstaionId()
+                    res.cookie("piyushAgentId_1",id,{
+                    httpOnly:true,
+                    sameSite:'lax',
+                    secure:false,
+                    maxAge: 30 * 1000 // 30 second
+               })
+
+                return await main(
+                    piyushAgent,
+                    piyushGuardRailAgent,
+                    PIYUSH_SIR_SYSTEM_PROMPT,
+                    res,id,message)
+
+           }
+              return await main(
+                piyushAgent,
+                piyushGuardRailAgent,
+                PIYUSH_SIR_SYSTEM_PROMPT,
+                res,personaId_1,message)
+            }
+           }
+
+})
+
+export default app;
