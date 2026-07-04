ce1cbc6c91da9a12e8ae934b8f721e9a5cf01bb8
MEHULARORA11 <mehularora505@gmail.com>
2026-07-04 05:31:29 +0530

    feat:setup  piyush sir's agent

---
+++ backend/src/agents/piyush/agent.ts
@@ -0,0 +1,103 @@
+import {Agent,run} from '@openai/agents'
+import readline from 'readline'
+import {OpenAI} from 'openai'
+import dotenv from 'dotenv'
+import { resolve, dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import {mainAgentInstruction} from './instruction.ts'
+import {guardRailAgent} from './guardrailAgent.ts'
+import {SYSTEM} from './Persona.ts'
+import{
+    weatherTool,
+    sendEmailToUserTool,
+    youtubeVideoSearchingTool,
+    youtubePlaylistSearchingTool
+} from './subTools.ts'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+dotenv.config({ path: resolve(__dirname, '../../../.env') })
+
+const client = new OpenAI()
+
+const {id} = await client.conversations.create({})
+
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+})
+const model = 'gpt-4o-mini'
+
+
+const piyushAgent = new Agent({
+    name:`Piyush's Agent`,
+    model,
+    instructions: mainAgentInstruction,
+    tools:[
+    weatherTool,
+    sendEmailToUserTool,
+    youtubeVideoSearchingTool,
+    youtubePlaylistSearchingTool
+    ]
+})
+
+
+while(true){
+    const question:string = await askQuestion("Ask Question: ")
+    if(question.toLowerCase() === 'exit'){
+        console.log('exiting...')
+        break;
+    }
+
+  process.stdout.write('ChatBot: ')
+  try {
+    await main(question)
+  } catch (error) {
+    console.log(error)
+  }
+  console.log('\n')
+
+}
+rl.close()
+
+async function main(question:string){
+
+    const guardRailResponse = await run(guardRailAgent,[
+        {
+        role:"user",
+        content:question
+       }
+])
+
+if(!guardRailResponse?.finalOutput?.isValidQuery){
+throw new Error(`Invalid Querry , due to Reason => ${guardRailResponse?.finalOutput?.reason}`)
+}
+    
+ const response = await run(piyushAgent,[
+        {
+        role:"system",
+        content:SYSTEM
+       },
+        {
+        role:"user",
+        content:question
+       }
+],{
+    stream:true,
+    conversationId:id
+})
+
+const streamOutput = response.toTextStream()
+
+for await (const chunk of streamOutput){
+    process.stdout.write(chunk)
+}
+
+}
+
+async function askQuestion(querry:string = ''):Promise<string>{
+  return new Promise((resolve) => {
+    rl.question(querry,(answer) => {
+        resolve(answer)
+    })
+  })
+}
