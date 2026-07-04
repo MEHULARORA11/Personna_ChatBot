0dc8dc4b484eafb7e7a3e38739a3af9a6d0ca5b0
MEHULARORA11 <mehularora505@gmail.com>
2026-07-05 03:18:03 +0530

    feat:configure a  node http server

---
+++ backend/server.ts
@@ -0,0 +1,16 @@
+import Application from './src/app.ts'
+import http from 'node:http'
+import 'dotenv/config'
+
+const PORT = process.env?.PORT
+
+async function main(){
+    const server = http.createServer(Application)
+    server.listen(PORT,() => {
+        console.log(`Server is running on Port ${PORT}`);
+    })
+}
+
+await main()
+
+
